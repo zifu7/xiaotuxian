4a5dd6f171e5ad05c839be591337eda92f820c63
echoe <echoe@example.com>
2026-04-21 11:24:57 +0800

    home-图片懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,22 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from "@vueuse/core";
+export const lazyPlugin = {
+  install(app) {
+    app.directive("lazy", {
+      mounted(el, binding) {
+        // el是指令绑定的元素 img
+        // binding.value是指令的值 图片地址 url
+        console.log(el, binding.value);
+        // 当图片进入视口区域则为true,否则为false
+        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+          console.log(isIntersecting);
+          if (isIntersecting) {
+            el.src = binding.value;
+            // 停止监听
+            stop();
+          }
+        });
+      },
+    });
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { useIntersectionObserver } from "@vueuse/core";
 import App from "./App.vue";
 import router from "./router";
 // 引入初始化样式
@@ -9,21 +8,12 @@ import { getCategoryAPI } from "@/apis/testAPI";
 getCategoryAPI().then((res) => {
   console.log(res);
 });
+// 引入懒加载插件
+import { lazyPlugin } from "@/directives";
 const app = createApp(App);
-// 图片懒加载指令，当图片进入可视区域时才加载图片
-app.directive("lazy", {
-  mounted(el, binding) {
-    // el是指令绑定的元素 img
-    // binding.value是指令的值 图片地址 url
-    console.log(el, binding.value);
-    // 当图片进入视口区域则为true,否则为false
-    useIntersectionObserver(el, ([{ isIntersecting }]) => {
-      console.log(isIntersecting);
-    });
-  },
-});
-app.use(createPinia());
-app.use(router);
 
 // 定义全局指令
+app.use(createPinia());
+app.use(router);
+app.use(lazyPlugin);
 app.mount("#app");
