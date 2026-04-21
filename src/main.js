296afda27de8f904ea41fcf9ed74565c21c8a9fe
echoe <echoe@example.com>
2026-04-21 11:11:46 +0800

    home-图片懒加载的实现

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import { useIntersectionObserver } from "@vueuse/core";
 import App from "./App.vue";
 import router from "./router";
 // 引入初始化样式
@@ -10,6 +10,20 @@ getCategoryAPI().then((res) => {
   console.log(res);
 });
 const app = createApp(App);
+// 图片懒加载指令，当图片进入可视区域时才加载图片
+app.directive("lazy", {
+  mounted(el, binding) {
+    // el是指令绑定的元素 img
+    // binding.value是指令的值 图片地址 url
+    console.log(el, binding.value);
+    // 当图片进入视口区域则为true,否则为false
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      console.log(isIntersecting);
+    });
+  },
+});
 app.use(createPinia());
 app.use(router);
+
+// 定义全局指令
 app.mount("#app");
