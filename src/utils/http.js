c3371981e59f6e29373ad4480efaeae5e2077698
echoe <echoe@example.com>
2026-05-06 10:18:15 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+import { ElMessage } from "element-plus";
+import { useUserStore } from "../stores/user";
 // 创建axios实例
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -9,6 +10,13 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    //从pinia获取token数据
+    const userStore = useUserStore();
+    //按照后端要求拼接token数据
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e),
