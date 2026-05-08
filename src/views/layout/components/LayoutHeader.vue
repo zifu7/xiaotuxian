b22b596d796d613b05baa966833a432e9e4d2060
echoe <echoe@example.com>
2026-05-08 12:23:37 +0800

    头部购物车-列表渲染和数量渲染

--- src/views/layout/components/LayoutHeader.vue
+++ src/views/layout/components/LayoutHeader.vue
@@ -1,6 +1,7 @@
 <script setup>
 // 调用接口函数
 import { useCategoryStore } from "@/stores/category";
+import HeaderCart from "./HeaderCart.vue";
 const categoryStore = useCategoryStore();
 </script>
 <template>
@@ -25,6 +26,7 @@ const categoryStore = useCategoryStore();
         <input type="text" placeholder="搜一搜" />
       </div>
       <!-- 头部购物车 -->
+      <HeaderCart />
     </div>
   </header>
 </template>
