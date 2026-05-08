a6064bb2b33ace5c3640ca36b39e1938fd8deb80
echoe <echoe@example.com>
2026-05-08 12:38:34 +0800

    本地-头部购物车的删除

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -6,7 +6,7 @@ export const useCartStore = defineStore(
   () => {
     //定义state-cartList
     const cartList = ref([]);
-    //定义actions=addCart
+    //定义actions-addCart
     const addCart = (goods) => {
       //将商品添加到购物车
       //判断购物车中是否已经有该商品,如果有,数量加1,如果没有,push到购物车
@@ -17,9 +17,16 @@ export const useCartStore = defineStore(
         cartList.value.push(goods);
       }
     };
+    //删除购物车中的商品
+    const delCart = (skuId) => {
+      //找到要删除的数组下标，用splice删除
+      const idx = cartList.value.findIndex((item) => item.skuId === skuId);
+      cartList.value.splice(idx, 1);
+    };
     return {
       cartList,
       addCart,
+      delCart,
     };
   },
   //持久化数据到localStorage中,刷新页面时数据不丢失
